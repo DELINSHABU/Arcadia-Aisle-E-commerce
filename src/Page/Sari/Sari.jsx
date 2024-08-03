6123bbe68f36c3adc5d41f8167d4465d81fe59a6
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-08-03 18:39:13 +0600

    shirt sari kurta route setup empty route

--- src/Page/Sari/Sari.jsx
+++ src/Page/Sari/Sari.jsx
@@ -1,19 +1,10 @@
+import Empty from "../../Components/Empty"
 
 
 function Sari() {
   return (
     <div>
-            <p className='text-3xl text-center mx-auto py-2'>Your Sari is empty</p>
-            <div className="flex w-52 flex-col gap-4  mx-auto">
-                <div className="flex items-center gap-4">
-                    <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-                    <div className="flex flex-col gap-4">
-                        <div className="skeleton h-4 w-20"></div>
-                        <div className="skeleton h-4 w-28"></div>
-                    </div>
-                </div>
-                <div className="skeleton h-32 w-full"></div>
-            </div>
+            <Empty name="Sari"></Empty>
         </div>
   )
 }
